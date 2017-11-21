c41adbf71468b31664f992968b0fd06d2b8d2c54
GuangchenW <32748931+GuangchenW@users.noreply.github.com>
2017-11-21 16:24:21 -0500

    Added servo stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDriveTeleOp.java
@@ -2,8 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
@@ -13,10 +18,13 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name="Omni Driving Mode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 public class OmniDriveTeleOp extends OpMode{
 
-    private DcMotor leftWheelF, leftWheelB, rightWheelF, rightWheelB;
-    private float leftX1;
-    private float leftY1;
-    private float rightX1;
+    private DcMotor leftWheelF, leftWheelB, rightWheelF, rightWheelB, linearSlide;
+    private Servo arm;
+    private CRServo harvesterL, harvesterR;
+    private ColorSensor armColor;
+    private GyroSensor gyro;
+    private float leftX1,leftY1, rightX1, leftY2;
+
     private boolean forward () {
         if (leftY1 > 0.9) {
             return true;
@@ -50,9 +58,18 @@ public class OmniDriveTeleOp extends OpMode{
         leftWheelB = hardwareMap.dcMotor.get("LWB");
         rightWheelF = hardwareMap.dcMotor.get("RWF");
         rightWheelB = hardwareMap.dcMotor.get("RWB");
+        linearSlide = hardwareMap.dcMotor.get("LS");
+
+        harvesterL = hardwareMap.crservo.get("HL");
+        harvesterR = hardwareMap.crservo.get("HR");
+        arm = hardwareMap.servo.get("arm");
+
+        armColor = hardwareMap.colorSensor.get("AC");
+        gyro = hardwareMap.gyroSensor.get("Gyro");
 
         rightWheelF.setDirection(DcMotorSimple.Direction.REVERSE);
         rightWheelB.setDirection(DcMotorSimple.Direction.REVERSE);
+        //harvesterR.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
@@ -64,11 +81,17 @@ public class OmniDriveTeleOp extends OpMode{
         leftX1 = gamepad1.left_stick_x;
         leftY1 = gamepad1.left_stick_y;
         rightX1 = gamepad1.right_stick_x;
+        leftY2 = gamepad2.left_stick_y;
 
-        Restriction();
-        Movement();
-        Rotate();
+        linearSlide.setPower(leftY2);
+        harvesterL.setPower((gamepad1.left_trigger - gamepad2.right_trigger) * time);
+        harvesterR.setPower((gamepad1.left_trigger - gamepad2.right_trigger) * time);
 
+        if (rightX1 == 0) {
+            Restriction();
+        } else {
+            Rotate();
+        }
     }
 
     @Override
@@ -87,6 +110,20 @@ public class OmniDriveTeleOp extends OpMode{
         rightWheelB.setPower(leftY1 - leftX1);
     }
 
+    private void MoveF (double power) {
+        leftWheelF.setPower(power);
+        leftWheelB.setPower(power);
+        rightWheelF.setPower(power);
+        rightWheelB.setPower(power);
+    }
+
+    private void MoveS (double power) {
+        leftWheelF.setPower(-power);
+        leftWheelB.setPower(power);
+        rightWheelF.setPower(-power);
+        rightWheelB.setPower(power);
+    }
+
     private void Rotate () {
         leftWheelF.setPower(rightX1);
         leftWheelB.setPower(rightX1);
@@ -96,10 +133,11 @@ public class OmniDriveTeleOp extends OpMode{
 
     private void Restriction () {
         if (forward() || backward()) {
-            leftX1 = 0;
-        }
-        if (left() || right()) {
-            leftY1 = 0;
+            MoveF(leftY1);
+        }else if (left() || right()) {
+            MoveS(leftX1);
+        } else {
+            Movement();
         }
     }
 }
